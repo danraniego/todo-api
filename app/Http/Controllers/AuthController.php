c1be454ad7704ce87e46827b2ab2ca8cf6309766
danraniego <danraniego@gmail.com>
2022-11-23 21:27:09 +0800

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Validator;
+
+class AuthController extends Controller {
+    /**
+     * User Login
+     */
+    public function login(Request $request) {
+
+        $responseData = [
+            'success' => false,
+            'message' => '',
+            'data'    => null
+        ];
+
+
+        $validator = Validator::make($request->all(), [
+            'email'     =>  'required|email',
+            'password'  =>  'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            $responseData['message'] = $validator->errors()->first();
+
+
+            return response($responseData, 400);
+        }
+
+        // Check Email
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            $responseData['message'] = 'Invalid email and password';
+            return response($responseData, 400);
+        }
+
+        $responseData['success'] = true;
+        $responseData['message'] = 'Authentication successful!';
+        $responseData['data'] = [
+            'user'  => $user,
+            'token' => $user->createToken('todo')->plainTextToken
+        ];
+
+        return response($responseData, 200);   
+    }
+
+
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Dan Walker',
+                'email' => 'danraniego@gmail.com',
+                'password' => bcrypt('admin123'),
+            ]
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
