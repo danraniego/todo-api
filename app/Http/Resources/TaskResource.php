9323d5688086a8eb0a4b46c30dcaaf386121bd37
MarkRywell <gopro855@gmail.com>
2022-11-30 20:37:51 +0800

    minor change

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -19,6 +19,7 @@ class TaskResource extends JsonResource
             'name' => $this->name,
             'details' => $this->details,
             'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
         ];
     }
 }
